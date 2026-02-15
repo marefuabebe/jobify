6ef89d7fd05fed796728d6de64fc6c426f060184
marefuabebe <abebemarefu266@gmail.com>
2026-02-15 23:14:00 +0300

    test: add unit and integration tests

---
+++ src/test/java/com/webapp/jobportal/JobportalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.webapp.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+//http://localhost:8081
